7655453bb721dc32fd5c16ccc00ce3b02ad3e1e0
rjasmine29 <jasmine.r@hotmail.com>
2021-11-17 22:57:38 +0000

    updated .js

--- client/index.js
+++ client/index.js
@@ -15,3 +15,20 @@ function search() {
     window.location.href = "https://www.google.com/search?q=" + input + "&rlz=1C1CHBF_en-GBGB973GB973&sxsrf=AOaemvLcYexzonMp7DbwEWR87V_Y-S63Mg%3A1637187919582&ei=T4GVYZjfIqXnsAeqo7OgAQ&gs_ssp=eJzj4tTP1TcwqUw3SFdgNGB0YPDiKs6uVCguyC8qKQYAYUEHwg&oq=sky&gs_lcp=Cgdnd3Mtd2l6EAMYADIKCC4QxwEQ0QMQJzIECCMQJzIECC4QQzIKCC4QxwEQowIQQzINCC4QsQMQxwEQ0QMQQzIKCAAQsQMQgwEQQzIECC4QQzIECAAQQzIOCC4QgAQQsQMQxwEQrwEyBAgAEEM6BwgjELADECc6BwgAEEcQsAM6EAguEMcBEKMCEMgDELADEEM6CgguEMgDELADEEM6DgguEIAEELEDEMcBEKMCOgsIABCABBCxAxCDAToHCAAQsQMQQzoKCC4QxwEQ0QMQQ0oFCDgSATFKBAhBGABQ7R9Yky5g7TloAnACeACAAV-IAdECkgEBNJgBAKABAcgBD8ABAQ&sclient=gws-wiz"
 }
 
+
+/* let randomSearch = document.querySelector('#randomSearch');
+randomSearch.onclick = function (event) {
+    getRandom();
+}
+
+async function getRandom() {
+    try {
+        let response = await fetch(`http://localhost:2000/quotes/random`);
+        let url = await response.text();
+        window.location.href = url;
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+*/
